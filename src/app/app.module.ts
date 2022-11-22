bfdbcc2af836c0fe74a1a48c1f042f748d2c58c5
Brian-Weloba <bweloba@gmail.com>
2022-11-22 17:44:34 +0300

    Finish UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatchesComponent } from './components/matches/matches.component';
+import { StandingsComponent } from './components/standings/standings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MatchesComponent,
+    StandingsComponent
   ],
   imports: [
     BrowserModule
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': [
+        '"Quicksand"',
+        'Roboto',
+      ],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
