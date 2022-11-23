2cf1364bc19c9a2e0625951f50ce60d3a10d8faa
Brian-Weloba <bweloba@gmail.com>
2022-11-23 11:26:55 +0300

    Change Url

--- src/app/services/standing.service.ts
+++ src/app/services/standing.service.ts
@@ -17,7 +17,7 @@ export class StandingService {
         .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzdjOTY4MDQ4NzA5MjMzZmQ5NGViNGYiLCJpYXQiOjE2NjkxMDk0OTQsImV4cCI6MTY2OTE5NTg5NH0.X9l_m2ykjSqH__UBU-EkpTEP9oE9nDnGAwjOqzZPc8E')
     }
     return new Promise<void>((resolve, reject) => {
-      this.http.get<any>('http://api.cup2022.ir/api/v1/standings', header).toPromise().then(
+      this.http.get<any>('https://http-parse.herokuapp.com/standings', header).toPromise().then(
         (result) => {
           this.tables = result.data;
           console.log(this.tables);
