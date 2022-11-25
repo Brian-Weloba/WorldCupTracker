afd9705dcf9622a33a4e3c85cbf47f753520864d
Brian-Weloba <bweloba@gmail.com>
2022-11-25 13:05:18 +0300

    Fix bug

--- src/app/components/standings/standings.component.ts
+++ src/app/components/standings/standings.component.ts
@@ -42,7 +42,7 @@ export class StandingsComponent implements OnInit{
           let time = this.live?.time - this.live?.match.time?.start;
           this.minutes = Math.floor(time / 60);
           if(this.minutes>45){
-            this.minutes = '45' +(this.minutes-45);
+            this.minutes = '45+' +(this.minutes-45);
           }
         } else if (this.live?.match.status.description === '2nd half') {
           let time = this.live?.time - this.live?.match.time?.period;
