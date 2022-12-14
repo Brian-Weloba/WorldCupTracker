7dcb036804d2ccadf450473f23a46364e67c7431
Brian-Weloba <bweloba@gmail.com>
2022-12-14 23:26:05 +0300

    Add loader

--- src/app/components/matches/matches.component.ts
+++ src/app/components/matches/matches.component.ts
@@ -1,35 +1,53 @@
-import {Component, OnInit} from '@angular/core';
-import {MatchService} from "../../services/match.service";
-import {Match} from "../../classes/match";
+import { Component, OnInit } from '@angular/core';
+import { MatchService } from '../../services/match.service';
+import { Match } from '../../classes/match';
 
 @Component({
   selector: 'app-matches',
   templateUrl: './matches.component.html',
-  styleUrls: ['./matches.component.css']
+  styleUrls: ['./matches.component.css'],
 })
 export class MatchesComponent implements OnInit {
   matches: Match[] | undefined;
+  loading = false;
+  err = false;
 
   ngOnInit(): void {
-    this.getMatches()
+    this.getMatches();
   }
 
   constructor(private matchService: MatchService) {
-    let match: Match = new Match("2", "A", 'TRUE', '1', '11/23/2022 22.00', 'ht', 'group', 'Argentina', 'Spain', '', '', '1');
+    let match: Match = new Match(
+      '2',
+      'A',
+      'TRUE',
+      '1',
+      '11/23/2022 22.00',
+      'ht',
+      'group',
+      'Argentina',
+      'Spain',
+      '',
+      '',
+      '1'
+    );
     this.matches?.push(match);
   }
 
-
-  private async getMatches() {
+  public async getMatches() {
+    this.loading = true;
     await this.matchService.getMatches().then(
       () => {
         this.matches = this.matchService.matches;
-        for(let match of this.matches){
+        for (let match of this.matches) {
           match.local_date = Date.parse(match.local_date);
         }
       },
       (error) => {
-        console.log(error)
-      })
+        console.log(error);
+        this.err=true
+      }
+    );
+    this.loading = false;
   }
 }
--- src/app/components/standings/standings.component.ts
+++ src/app/components/standings/standings.component.ts
@@ -1,33 +1,37 @@
-import {Component, OnInit} from '@angular/core';
-import {StandingService} from "../../services/standing.service";
-import {Table} from "../../classes/table";
-import {Live} from "../../classes/live";
-import {LiveScoreService} from "../../services/live-score.service";
+import { Component, OnInit } from '@angular/core';
+import { StandingService } from '../../services/standing.service';
+import { Table } from '../../classes/table';
+import { Live } from '../../classes/live';
+import { LiveScoreService } from '../../services/live-score.service';
 
 @Component({
   selector: 'app-standings',
   templateUrl: './standings.component.html',
-  styleUrls: ['./standings.component.css']
+  styleUrls: ['./standings.component.css'],
 })
-export class StandingsComponent implements OnInit{
-   tables: Table[] = [];
-  groups: string[] = ['A','B','C','D','E','F','G','H'];
+export class StandingsComponent implements OnInit {
+  tables: Table[] = [];
+  groups: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
   live: Live | undefined;
-  minutes: any
-   match_time: any;
+  minutes: any;
+  match_time: any;
+  loading = false;
 
   ngOnInit(): void {
-    this.fetchStandings()
-    this.fetchLive()
-    window.setInterval(() => {
-      this.fetchLive()
-    }, 10000);
+    this.fetchStandings();
+    this.fetchLive();
+    if (this.live?.match.time.period != null) {
+      window.setInterval(() => {
+        this.fetchLive();
+        console.log('fetch');
+      }, 10000);
+    }
   }
 
-  constructor(private standingsService:StandingService,private liveScore: LiveScoreService) {
-  }
-
-
+  constructor(
+    private standingsService: StandingService,
+    private liveScore: LiveScoreService
+  ) {}
 
   ngOnDestroy(): void {
     // clearInterval(intervalId)
@@ -37,43 +41,46 @@ export class StandingsComponent implements OnInit{
     await this.liveScore.getLiveScore().then(
       () => {
         this.live = this.liveScore.live;
-        this.match_time = new Date(this.live?.match.time?.start*1000).toLocaleString();
+        this.match_time = new Date(
+          this.live?.match.time?.start * 1000
+        ).toLocaleString();
         if (this.live?.match.status.description === '1st half') {
           let time = this.live?.time - this.live?.match.time?.start;
           this.minutes = Math.floor(time / 60);
-          if(this.minutes>45){
-            this.minutes = '45+' +(this.minutes-45);
+          if (this.minutes > 45) {
+            this.minutes = '45+' + (this.minutes - 45);
           }
         } else if (this.live?.match.status.description === '2nd half') {
           let time = this.live?.time - this.live?.match.time?.period;
-          this.minutes = Math.floor(time / 60)+45;
-          if(this.minutes>90){
-            this.minutes = '90+'+(this.minutes-90);
+          this.minutes = Math.floor(time / 60) + 45;
+          if (this.minutes > 90) {
+            this.minutes = '90+' + (this.minutes - 90);
           }
         } else if (this.live?.match.status.description === 'Halftime') {
-          this.minutes = 'HT'
-        }else if(this.live?.match.status.description === 'Ended'){
-          this.minutes = 'FT'
+          this.minutes = 'HT';
+        } else if (this.live?.match.status.description === 'Ended') {
+          this.minutes = 'FT';
         }
       },
       (error) => {
-        console.log(error)
-      })
+        console.log(error);
+      }
+    );
   }
 
-  async fetchStandings(){
+  async fetchStandings() {
+    this.loading = true;
     await this.standingsService.fetchTables().then(
       () => {
         this.tables = this.standingsService.tables;
-        for (let table of this.tables){
-          table.teams.sort((a,b)=>b.pts-a.pts);
+        for (let table of this.tables) {
+          table.teams.sort((a, b) => b.pts - a.pts);
         }
       },
       (error) => {
-        console.log(error)
-      })
+        console.log(error);
+      }
+    );
+    this.loading = false;
   }
-
-
-
 }
