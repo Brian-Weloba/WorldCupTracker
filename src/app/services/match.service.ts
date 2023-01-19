c13a2164693daaf23e892f609ed4b9441adc4910
brian-weloba <bweloba@gmail.com>
2023-01-19 13:40:19 +0000

    Change URL

--- src/app/services/match.service.ts
+++ src/app/services/match.service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from '@angular/core';
-import {Match} from "../classes/match";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {lastValueFrom} from "rxjs";
+import { Injectable } from '@angular/core';
+import { Match } from "../classes/match";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { lastValueFrom } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -18,15 +18,15 @@ export class MatchService {
       headers: new HttpHeaders()
         .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzdjOTY4MDQ4NzA5MjMzZmQ5NGViNGYiLCJpYXQiOjE2NjkxMDk0OTQsImV4cCI6MTY2OTE5NTg5NH0.X9l_m2ykjSqH__UBU-EkpTEP9oE9nDnGAwjOqzZPc8E')
     };
-    return  new Promise<void>(async (resolve, reject) => {
-      await lastValueFrom(this.http.get<any>('https://projects.saturdev.tech/test/match', header)).then(
+    return new Promise<void>(async (resolve, reject) => {
+      await lastValueFrom(this.http.get<any>('https://projects.saturdev.tech/worldcupapi/matches', header)).then(
         (result) => {
 
           this.matches = result?.data;
           // console.log(this.matches);
           resolve();
         },
-        (err)=>{
+        (err) => {
           console.log(err)
           reject();
         }
