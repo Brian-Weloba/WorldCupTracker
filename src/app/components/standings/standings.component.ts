aa16a3d628c892e6dd71fc3f798c40cd75aba769
Brian-Weloba <bweloba@gmail.com>
2023-01-22 12:43:41 +0300

    Remove particles

--- src/app/components/standings/standings.component.ts
+++ src/app/components/standings/standings.component.ts
@@ -161,16 +161,16 @@ export class StandingsComponent implements OnInit {
     detectRetina: true,
 };
 
-particlesLoaded(container: Container): void {
-    // console.log(container);
-}
+// particlesLoaded(container: Container): void {
+//     // console.log(container);
+// }
 
-async particlesInit(engine: Engine): Promise<void> {
-    // console.log(engine);
+// async particlesInit(engine: Engine): Promise<void> {
+//     // console.log(engine);
 
-    // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
-}
+//     // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
+//     // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+//     // starting from v2 you can add only the features you need reducing the bundle size
+//     await loadFull(engine);
+// }
 }
